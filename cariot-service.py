9145989b37a4359e52e3fb91849b08d4c860b8cd
Ralph Grewe <ralph@vehicle-hacks.de>
2022-04-15 14:21:27 +0200

    Moved AWS part to a separate thread

--- cariot-service.py
+++ cariot-service.py
@@ -23,32 +23,58 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from awscrt import io
 from awsiot import mqtt_connection_builder
 
-import threading
+from threading import Thread
 import time
 import json
+
+class aws_iot:
+    def __init__(self):
+        self.aws_running = False
+        self.thing_config = ""
+
+    def start(self, config):
+        self.aws_running = True
+        self.thing_config = config['thing']
+        self.aws_thread = Thread(target=self.aws_loop) 
+        self.aws_thread.start()
+
+    def aws_loop(self):
+        event_loop_group = io.EventLoopGroup(1)
+        host_resolver = io.DefaultHostResolver(event_loop_group)
+        client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+
+        aws_iot_connection = mqtt_connection_builder.mtls_from_path(
+            endpoint = self.thing_config['endpoint'],
+            client_id = self.thing_config['clientId'],
+            client_bootstrap = client_bootstrap,
+            cert_filepath = self.thing_config['certfile'],
+            pri_key_filepath = self.thing_config['keyfile'],
+            ca_filepath = self.thing_config['rootCaFile'],
+            clean_session = False
+        )
+
+        aws_iot_connect_future = aws_iot_connection.connect()
+        aws_iot_connect_future.result()
+        print("Connected")
+
+        while (self.aws_running == True):
+            print("A")
+            time.sleep(1)
+
+        aws_iot_disconnect_future = aws_iot_connection.disconnect()
+        aws_iot_disconnect_future.result()
+        print("Disconnected!")                    
+
+    def stop(self):
+        self.aws_running = False
+        self.aws_thread.join(5)
+
 
 config_file = open('./cariot-config.json')
 cariot_config = json.load(config_file)
 
-# We should
-event_loop_group = io.EventLoopGroup(1)
-host_resolver = io.DefaultHostResolver(event_loop_group)
-client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
-
-aws_iot_connection = mqtt_connection_builder.mtls_from_path(
-    endpoint = cariot_config['thing']['endpoint'],
-    client_id = cariot_config['thing']['clientId'],
-    client_bootstrap = client_bootstrap,
-    cert_filepath = cariot_config['thing']['certfile'],
-    pri_key_filepath = cariot_config['thing']['keyfile'],
-    ca_filepath = cariot_config['thing']['rootCaFile'],
-    clean_session = False
-)
-
-aws_iot_connect_future = aws_iot_connection.connect()
-aws_iot_connect_future.result()
-print("Connected")
-
-aws_iot_disconnect_future = aws_iot_connection.disconnect()
-aws_iot_disconnect_future.result()
-print("Disconnected!")
+aws = aws_iot()
+aws.start(cariot_config)
+time.sleep(10)
+print("Stopping thread")
+aws.stop()
